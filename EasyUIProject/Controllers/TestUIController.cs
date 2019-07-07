c17218b316ef6c6f57a52a7ee71430a5f7b23c19
gamecc666 <995611928@qq.com>
2019-07-07 15:58:52 +0800

    init project

---
+++ EasyUIProject/Controllers/TestUIController.cs
@@ -0,0 +1,23 @@
+﻿using EasyUIProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyUIProject.Controllers
+{
+    public class TestUIController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult EastUIGetData()
+        {
+            var _node = new NodeModel();
+            _node.id = 1;
+            _node.text = "root1";
+
+
+            return Json(_node);
+        }
+    }
+}
