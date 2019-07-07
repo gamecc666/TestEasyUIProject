fe51f77217bddc01d2f641dc120a720c44bd7e75
gamecc666 <995611928@qq.com>
2019-07-08 01:21:04 +0800

    前后台通信init

--- EasyUIProject/Controllers/TestUIController.cs
+++ EasyUIProject/Controllers/TestUIController.cs
@@ -1,23 +1,35 @@
 ﻿using EasyUIProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Web.Script.Serialization;
 
 namespace EasyUIProject.Controllers
 {
     public class TestUIController : Controller
     {
+        private readonly ILogger _logger;
+        public TestUIController(ILogger<TestUIController> logger)
+        {
+            _logger = logger;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
-        [HttpGet]
-        public IActionResult EastUIGetData()
+        [HttpPost]
+        public ActionResult GetUserTreeJson()
         {
-            var _node = new NodeModel();
-            _node.id = 1;
-            _node.text = "root1";
-
-
-            return Json(_node);
+            var _node = new EasyTreeData("1", "Root");          
+            string json = ToJson(_node);
+            _logger.LogWarning("--------------gamecc----");
+            return Content(json);
+        }
+        //生成Json
+        protected string ToJson(object obj)
+        {
+            string jsonData = (new JavaScriptSerializer()).Serialize(obj);
+            return jsonData;
         }
     }
 }
---
+++ EasyUIProject/Models/EasyTreeData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EasyUIProject.Models
+{
+    [Serializable]
+    public class EasyTreeData
+    {
+        public string id { get; set; }
+        public string text { get; set; }
+        public string state { get; set; }
+        public string iconCls { get; set; }
+        public List<EasyTreeData> children { get; set; }
+        public EasyTreeData()
+        {
+            this.children = new List<EasyTreeData>();
+            this.state = "closed";
+        }
+        public EasyTreeData(string id,string text,string state="closed"):this()
+        {
+            this.id = id;
+            this.text = text;
+            this.state = state;
+        }
+        public EasyTreeData(int id, string text, string state = "open", List<EasyTreeData> children = null): this()
+        {
+            this.id = id.ToString();
+            this.text = text;
+            this.state = state;
+            this.children = children;
+
+        }
+    }
+}
